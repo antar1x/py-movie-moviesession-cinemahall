0f60124860d9672d6131d7c617994ad3203720ed
David <meeddry@gmail.com>
2026-05-03 14:02:09 +0300

    created_movie_service

---
+++ services/movie.py
@@ -0,0 +1,32 @@
+from db.models import Movie, Genre, Actor
+
+def get_movies(genres_ids=None, actors_ids=None):
+    if genres_ids and actors_ids:
+        return Movie.objects.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
+        )
+    if genres_ids:
+        return Movie.objects.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        return Movie.objects.filter(actors__id__in=actors_ids)
+    return Movie.objects.all()
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+        movie_title,
+        movie_description,
+        genres_ids=None,
+        actors_ids=None
+) -> Movie:
+    movie_ = Movie.objects.create(title=movie_title, description=movie_description)
+    if genres_ids:
+        movie_.genres.add(*genres_ids)
+
+    if actors_ids:
+        movie_.actors.add(*actors_ids)
+
+    return movie_
